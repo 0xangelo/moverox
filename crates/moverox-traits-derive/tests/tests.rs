efaaf2a3da361f7c6daaf92603f56a8478b1abc6
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2025-07-24 22:44:51 -0500

    refactor(moverox-traits-derive): move sources for `trybuild` tests

--- crates/moverox-traits-derive/tests/tests.rs
+++ crates/moverox-traits-derive/tests/tests.rs
@@ -1,12 +1,12 @@
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
-    t.pass("tests/braced_struct.rs");
-    t.pass("tests/tuple_struct.rs");
-    t.pass("tests/generic_tuple_struct.rs");
-    t.pass("tests/generic_braced_struct.rs");
-    t.pass("tests/otw.rs");
-    t.pass("tests/no_type_bounds.rs");
-    t.compile_fail("tests/empty_braced_struct.rs");
-    t.compile_fail("tests/empty_tuple_struct.rs");
+    t.pass("tests/sources/braced_struct.rs");
+    t.pass("tests/sources/tuple_struct.rs");
+    t.pass("tests/sources/generic_tuple_struct.rs");
+    t.pass("tests/sources/generic_braced_struct.rs");
+    t.pass("tests/sources/otw.rs");
+    t.pass("tests/sources/no_type_bounds.rs");
+    t.compile_fail("tests/sources/empty_braced_struct.rs");
+    t.compile_fail("tests/sources/empty_tuple_struct.rs");
 }
