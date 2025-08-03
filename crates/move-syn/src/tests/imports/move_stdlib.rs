c7f86bb6ce02bda11963b8405f58615fcafa89d0
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2025-08-03 15:28:02 -0500

    fix(move-syn): handle `Self` imports

---
+++ crates/move-syn/src/tests/imports/move_stdlib.rs
@@ -0,0 +1,23 @@
+use super::*;
+
+test_files!("move-stdlib" {
+    address,
+    ascii,
+    bcs,
+    bit_vector,
+    bool,
+    debug,
+    fixed_point32,
+    hash,
+    macros,
+    option,
+    string,
+    type_name,
+    u128,
+    u64,
+    u8,
+    unit_test,
+    uq32_32,
+    uq64_64,
+    vector,
+});
