ac2e11874a32ff331ff19898c357b59ba9033057
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2025-12-22 05:55:27 -0300

    feat(move-syn): `Visibility` helpers

--- crates/move-syn/src/vis.rs
+++ crates/move-syn/src/vis.rs
@@ -11,6 +11,20 @@ pub enum Visibility {
     Private,
 }
 
+impl Visibility {
+    pub const fn is_public(&self) -> bool {
+        matches!(self, Self::Public)
+    }
+
+    pub const fn is_package(&self) -> bool {
+        matches!(self, Self::Package)
+    }
+
+    pub const fn is_private(&self) -> bool {
+        matches!(self, Self::Private)
+    }
+}
+
 impl Item {
     pub const fn visibility(&self) -> Visibility {
         let Some(vis) = &self.vis else {
